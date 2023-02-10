825221a6d11bba5a749a8d74bcd8da6158c21355
Kuyee Chang <kyec1118@gmail.com>
2023-02-10 11:11:58 +0200

    hamburger menu

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,12 +1,29 @@
 import "./navBar.scss";
+import { useState } from "react";
 import NavButton from "../UI/navButton/navButton";
 const NavBar = () => {
+	const [showNavBar, setShowNavBar] = useState(false);
+	const handleShowNavBar = () => {
+		setShowNavBar(!showNavBar);
+		console.log(showNavBar);
+	};
+
 	return (
-		<div className="navBar">
-			<NavButton btnName="About" />
-			<NavButton btnName="Experience" />
-			<NavButton btnName="Projects" />
-			<NavButton btnName="Contact" />
+		<div className="nav_bar">
+			<div
+				className={`menu_icon ${showNavBar && "close"}`}
+				onClick={handleShowNavBar}
+			>
+				<div class="btn-line"></div>
+				<div class="btn-line"></div>
+				<div class="btn-line"></div>
+			</div>
+			<div className={`nav_btns ${showNavBar && "active"}`}>
+				<NavButton btnName="About" />
+				<NavButton btnName="Experience" />
+				<NavButton btnName="Projects" />
+				<NavButton btnName="Contact" />
+			</div>
 		</div>
 	);
 };
