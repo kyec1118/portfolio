8b05c1fa354e1f1f875d1d42a0254e8a2031f50f
Kuyee Chang <kyec1118@gmail.com>
2023-03-17 18:40:32 +0800

    about section complete layout

--- src/components/section/secondSection/secondSection.js
+++ src/components/section/secondSection/secondSection.js
@@ -1,33 +1,62 @@
 import "./secondSection.scss";
+import { ReactComponent as Desk } from "../../../assets/desk.svg";
+import { ReactComponent as Dots } from "../../../assets/dotsWhite.svg";
+import Map from "../../../components/map/map.js";
+import profile from "../../../assets/images/profile.jpg";
+
 const SecondSection = () => {
 	return (
-		<section className="second" data-scroll-section>
-			<div
-				className="second_title"
-				data-scroll
-				data-scroll-direction="horizontal"
-				data-scroll-speed="-5"
-			>
-				Experience
-			</div>
-			{/* <div className="second_text_section">
+		<section className="about" id="About" data-scroll-section>
+			<Desk className="desk" />
+			<div className="about_title">
+				<Dots className="dots_white" />
 				<h1
+					className="about_title_text"
 					data-scroll
 					data-scroll-direction="horizontal"
-					data-scroll-speed="9"
+					data-scroll-speed="-2"
 				>
-					I Love React
+					About Me.
 				</h1>
-				<h1
-					data-scroll
-					data-scroll-direction="vertical"
-					data-scroll-speed="5" // Values provided here affect the animations
-				>
-					That's why I code every day
-				</h1>
-			</div> */}
-
-			<div className="image"></div>
+			</div>
+			<div className="about_content">
+				<div className="about_education">
+					<img
+						src={profile}
+						className="profile"
+						alt="profile"
+						data-scroll
+						data-scroll-direction="horizontal"
+						data-scroll-speed="-3"
+					/>
+					<div className="about_education_textbox">
+						<h2
+							className="about_education_title"
+							data-scroll
+							data-scroll-speed="3"
+						>
+							學歷
+						</h2>
+						<ul className="about_education_text">
+							<p className="about_education_master" data-scroll>
+								台灣大學資訊管理研究所
+							</p>
+							<p className="about_education_bachelor">中央大學資訊管理系</p>
+						</ul>
+					</div>
+				</div>
+				<div className="about_journey">
+					<span className="about_journey_wrap" data-scroll>
+						<span data-scroll data-scroll-delay="0.40" data-scroll-speed="2">
+							經
+						</span>
+						<span data-scroll data-scroll-delay="0.1" data-scroll-speed="2">
+							歷
+						</span>
+					</span>
+					<Map />
+				</div>
+			</div>
 		</section>
 	);
 };
