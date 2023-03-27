33dd0ea83746917f4c4af2516e57d816b6c04e73
Kuyee Chang <kyec1118@gmail.com>
2023-03-27 09:54:13 +0800

    updated contact and project section

--- src/components/section/contactSection/contactSection.js
+++ src/components/section/contactSection/contactSection.js
@@ -1,14 +1,62 @@
 import "./contactSection.scss";
-import EmailIcon from "../../../assets/Email 3.png";
 import ContactInfo from "../../UI/contactInfo/contactInfo";
+import { ReactComponent as ContactTitle } from "../../../assets/contactTitle.svg";
 import { ReactComponent as ContactBg } from "../../../assets/contactBg.svg";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import { Email } from "@mui/icons-material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
+import { useEffect } from "react";
 const ContactSection = () => {
+	const { scroll } = useLocomotiveScroll();
+	const target = document.getElementById("contact_svg");
+	useEffect(() => {
+		if (scroll && target) {
+			scroll.on("scroll", (instance) => {
+				console.log(instance);
+				if (instance.limit.y - instance.scroll.y < 500) {
+					target.style.transform =
+						"rotate(-" + (instance.limit.y - instance.scroll.y) / 8 + "deg)";
+				}
+			});
+		}
+	}, [scroll, target]);
 	return (
 		<div className="contact" id="Contact" data-scroll-section>
 			<ContactBg className="contact_bg" />
-			<div className="contact_list">
-				<ContactInfo icon={EmailIcon} info="dalec1118@gmail.com" />
-				<ContactInfo icon={EmailIcon} info="124356789" />
+
+			<div className="contact_title_wrapper" id="contact_svg">
+				<ContactTitle className="contact_svg" />
+			</div>
+			<div className="contact_context">
+				<div className="contact_list">
+					{" "}
+					<ContactInfo
+						img={Email}
+						info="dalec1118@gmail.com"
+						link="mailto: dalec1118@gmail.com"
+					/>
+					<ContactInfo
+						img={FacebookIcon}
+						info="Kuyee Chang"
+						link="https://www.facebook.com/kuyee.chang"
+					/>
+					<ContactInfo
+						img={LinkedInIcon}
+						info="Kuyee Chang"
+						link="https://www.facebook.com/kuyee.chang"
+					/>{" "}
+				</div>{" "}
+				<div className="resume_download">
+					<a
+						href="https://drive.google.com/file/d/1OWxc3imNVCRQcbdG8JBbqT0gSXf3p98z/view?usp=share_link"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="resume_title"
+					>
+						[ 完整履歷 ]
+					</a>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/section/projectSection/projectSection.js
+++ src/components/section/projectSection/projectSection.js
@@ -3,41 +3,56 @@ import { ReactComponent as Dots } from "../../../assets/dots.svg";
 import ProjectItem from "../../UI/projectItem/projectItem";
 import projectImg from "../../../assets/Portfolio.jpg";
 import MLL from "../../../assets/MeetLangLang.png";
+import furbo from "../../../assets/furbo.jpg";
 const ProjectSection = () => {
 	return (
 		<section className="project" id="Projects" data-scroll-section>
 			<Dots className="project_dots" />
-			<div
-				className="project_section_title"
-				data-scroll
-				data-scroll-direction="horizontal"
-				data-scroll-speed=""
-			>
-				Projects.
+			<div id="project_title_wrapper" className="project_title_wrapper">
+				<div
+					className="project_section_title"
+					data-scroll
+					data-scroll-sticky
+					data-scroll-target="#project_title_wrapper"
+				>
+					Projects.
+				</div>
+				<div className="project_items">
+					<ProjectItem
+						name="portfolio"
+						img={projectImg}
+						width="30vw"
+						height="auto"
+						title="我的個人網站"
+						text={[
+							"以 Adobe XD 進行 UI Design，並以 React 開發。利用 Locomotive Scroll 套件嘗試達成流暢的瀏覽體驗。其中也有使用 Adobe After Effect、Adobe Illustrator 製圖以及動畫。",
+							<br />,
+							"還在繼續更新中☺️",
+						]}
+					/>
+					<ProjectItem
+						name="mll"
+						img={MLL}
+						width="30vw"
+						height="auto"
+						title="遇見浪浪"
+						text={[
+							"碩一時與同學們進行的專案，一個認領養平台，提供橋樑讓愛心認養人們找到屬於自己的浪浪。",
+							<br />,
+							"利用React、Node.js、SQL、Docker進行開發， 功能包含Facebook SSO、雙語模式、Google Maps串接、討論區、寵物抽卡功能等等！",
+						]}
+					/>
+				</div>
 			</div>
-			<div className="project_items">
-				<ProjectItem
-					img={projectImg}
-					width="30vw"
-					height="auto"
-					title="Dale 個人網站"
-					text="利用 Adobe XD 進行 UI Design，並以 React.js開發。利用 Locomotive Scroll、
-GSAP 等 Scrolling 套件嘗試達成流暢的瀏覽體驗。其中利用Adobe After Effect、Adobe Illustrator 製圖以及動畫。"
-				/>
 
-				<ProjectItem
-					img={MLL}
-					width="30vw"
-					height="auto"
-					title="遇見浪浪"
-					text="碩一時與同學們進行的專案，一個認領養網頁平台！
-							提供橋樑讓愛心認養人們找到屬於自己的浪浪 :) 利用
-							React、Node.js、SQL、Docker進行開發， 功能包含Facebook
-							SSO、雙語模式、Google
-							Maps串接、討論區、寵物抽卡功能等，有興趣的話可以到這裡看實體網頁唷！"
-					reverse={true}
-				/>
-			</div>
+			<ProjectItem
+				name="furbo"
+				img={furbo}
+				width="40vw"
+				height="auto"
+				title="Furbo 官網"
+				text={["於 Tomofun 實習時參與公司網頁開發 🐕‍🦺"]}
+			/>
 		</section>
 	);
 };
