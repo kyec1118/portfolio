745d03727a89b02cb5c6cfef8681b613352acb6e
Kuyee Chang <kyec1118@gmail.com>
2023-03-27 09:50:13 +0800

    welcome section renew

--- src/components/section/welcomeSection/welcomeSection.js
+++ src/components/section/welcomeSection/welcomeSection.js
@@ -1,38 +1,47 @@
 import "./welcomeSection.scss";
 import { ReactComponent as BlueBlob } from "../../../assets/BlueBlob.svg";
-import { ReactComponent as PinkBlob } from "../../../assets/PinkBlob.svg";
-import portrait from "../../../assets/Portrait2.png";
+import { ReactComponent as DeskBg } from "../../../assets/deskbg.svg";
+import { ReactComponent as DeskFt } from "../../../assets/deskfront.svg";
+import { ReactComponent as Dots } from "../../../assets/dots.svg";
+import portrait from "../../../assets/girl_svg.gif";
+
 const WelcomeSection = () => {
 	return (
 		<section className="welcome" data-scroll-section>
 			<BlueBlob className="blueblob" />
-			<PinkBlob className="pinkblob" />
+			<DeskBg className="deskbg" />
+			<DeskFt className="deskft" />
 			<div className="welcome_text_section">
-				<div
+				<Dots className="dots" />
+				<h1
 					className="welcome_text_hello"
 					data-scroll
 					data-scroll-direction="horizontal"
-					data-scroll-speed="2"
+					data-scroll-speed="3"
+					data-scroll-position="top"
 				>
 					Hello! I'm Dale.
-				</div>
-				<div
+				</h1>
+				<p
 					className="welcome_text_intro"
 					data-scroll
 					data-scroll-direction="horizontal"
-					data-scroll-speed="1"
+					data-scroll-speed="2"
+					data-scroll-position="top"
 				>
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry's standard dummy text ever
-					since the 1500s, when an unknown printer took a galley of type and
-					scrambled it to make a type specimen book.
-				</div>
+					嗨！我是古宜，歡迎來到我的網頁 :)
+					<br />
+					我的過去經驗包含前後端開發、軟硬體測試、專案管理，也喜歡學習新技術 :)
+					<br />
+					若能透過這個頁面更認識我就太好了！
+				</p>
 			</div>
 			<div
 				className="welcome_image"
 				data-scroll
 				data-scroll-direction="horizontal"
-				data-scroll-speed="-2"
+				data-scroll-speed="-1"
+				data-scroll-position="top"
 			>
 				<img
 					src={portrait}
@@ -40,9 +49,11 @@ const WelcomeSection = () => {
 					data-scroll
 					data-scroll-direction="horizontal"
 					data-scroll-speed="-3"
+					data-scroll-position="top"
 				/>
 			</div>
 		</section>
 	);
 };
+
 export default WelcomeSection;
