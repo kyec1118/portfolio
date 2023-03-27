eaa6fbfdd9b7eb74bb8a1a478c7218bbc524b764
Kuyee Chang <kyec1118@gmail.com>
2023-03-27 09:51:36 +0800

    renew contact info display

--- src/components/UI/contactInfo/contactInfo.js
+++ src/components/UI/contactInfo/contactInfo.js
@@ -2,8 +2,15 @@ import "./contactInfo.scss";
 const ContactInfo = (props) => {
 	return (
 		<div className="contact_info">
-			<img src={props.icon} className="icon" />
-			<div className="text">{props.info}</div>
+			<props.img className="contact_info_icon" />
+			<a
+				href={props.link}
+				target="_blank"
+				rel="noopener noreferrer"
+				className="text"
+			>
+				{props.info}
+			</a>
 		</div>
 	);
 };
